3ab7f71c0713c2c66a234f03cb706bb61b1f16d9
chris-rattana <chris.rattana.investisseur@gmail.com>
2026-02-13 03:15:38 +0100

    Add README and main entrypoint

---
+++ main.py
@@ -0,0 +1,10 @@
+from calculator.calculator import add, subtract, multiply, divide
+
+def main():
+    print(add(2, 3))
+    print(subtract(10, 4))
+    print(multiply(6, 7))
+    print(divide(8, 2))
+
+if __name__ == "__main__":
+    main()
